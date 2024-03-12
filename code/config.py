bfce0df402dac72b9c0289d750cbfaef4997a42d
David Musafiri <david.musafiri@etu.univ-paris-diderot.fr>
2024-03-12 15:06:00 +0100

    Config

--- code/config.py
+++ code/config.py
@@ -2,11 +2,12 @@ from pathlib import Path
 
 def get_config():
     return{
-        "batch_size": 8,
+        "batch_size": 16,
         "num_epochs": 20,
         "lr": 10**-4,
         "seq_len": 500,
         "d_model": 512,
+        "dataset":"opus_books",
         "lang_src": "en",
         "lang_tgt": "fr",
         "model_folder": "weights",
